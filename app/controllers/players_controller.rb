f6decbdde4b9b20355ac7f8e4064a787f322a75a
A1on3R <angeldelacruz1026@gmail.com>
2021-10-26 02:39:04 -0400

    added an upload file page for the player average csv.

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -2,4 +2,28 @@ class PlayersController < ApplicationController
   def index
     @players = Player.all
   end
+
+  def upload_players
+  end
+
+
+
+  def import
+    require 'csv'
+    myfile = params[:file]
+    player_row_array = CSV.read(myfile.path)
+
+    #Clear the player db so i Overwrite instead of extend
+    Player.delete_all
+
+    CSV.foreach(myfile.path, headers: true) do |row|
+      Player.create!(row.to_hash)
+    end
+
+      redirect_to action: "index"
+    end
+  
+
+ 
+
 end
