81c73c64a773a8a9a90fe21b9a380cbb70ff551e
Angel <--global>
2024-08-23 23:56:58 -0400

    table_for gem and routes. App runs now

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,21 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  get "/players", to:"players#index"
+  get "/upload_players", to: "players#upload_players"
+  post "/upload_players", to: "players#import"
+  get "/teams", to:"teams#index"
+  get "/dvps", to:"dvp#index"
+  get "/upload_dvp", to: "dvp#upload_dvp"
+  post "/upload_dvp", to: "dvp#import_dvp"
+  get "/upload_salaries", to: "results#upload_salaries"
+  post "/upload_salaries", to: "results#import_salaries"
+  
+  get "/upload_minutes", to: "results#upload_minutes"
+  post "/upload_minutes", to: "results#set_minutes" 
+  
+  
+
+  resources :results
 end
